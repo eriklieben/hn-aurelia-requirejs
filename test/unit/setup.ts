769a0201c9781c40599758e7493c0f6b0ef317f7
Erik Lieben <erik.lieben@effectory.com>
2017-06-26 07:46:11 +0200

    chore(test): add jest as testing framework

--- test/unit/setup.ts
+++ test/unit/setup.ts
@@ -1,3 +1,8 @@
 import 'aurelia-polyfills';
-import {initialize} from 'aurelia-pal-browser';
+import {initialize} from 'aurelia-pal-nodejs';
 initialize();
+
+import 'whatwg-fetch';
+
+import {Container} from 'aurelia-dependency-injection';
+Container.instance = new Container();
