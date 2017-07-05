907c7f01ef25ca7d1d3647d8edb3dd8a54e52fe1
Erik Lieben <erik.lieben@effectory.com>
2017-07-05 20:16:31 +0200

    fix(images): cache cloudinary resources with sw

--- sw-source.js
+++ sw-source.js
@@ -7,3 +7,16 @@ hackernews.init(firebase, { watch: true });
 
 const workboxSW = new self.WorkboxSW();
 workboxSW.precache([]);
+
+workboxSW.router.registerNavigationRoute('/');
+
+workboxSW.router.registerRoute(
+  new RegExp('^https://res.cloudinary.com/'),
+  workboxSW.strategies.cacheFirst({
+    cacheName: 'images',
+    cacheExpiration: { maxEntries: 15 },
+    // The images are returned as opaque responses, with a status of 0.
+    // Normally these wouldn't be cached; here we opt-in to caching them.
+    cacheableResponse: { statuses: [0] }
+  })
+);
